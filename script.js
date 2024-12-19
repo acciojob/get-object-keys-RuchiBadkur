87f812e4ee929369065115f2f390731de168dc14
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-12-19 13:05:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,64 @@
 //your JS code here. If required.
-function Student(name){
-	this.name = name
-}
 
-const student = new Student("ruchi");
-// const student = new Student(name)
-student.getKeys = function(){
-	return Object.getOwnPropertyNames(student)
-// Object.getOwnPropertyNames(Student.prototype)
-}
 
-// console.log(student.getKeys())
-student.getKeys()
+const student = {
+	name: "ruchi"
+}
+Object.prototype.getKeys = function(){
+	return Object.keys(this)
+}
+console.log(student.getKeys());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // constructor
+// function Student(name){
+// 	this.name = name
+// }
+
+
+// // adding new key to the constructor via prototype
+// Student.prototype.getKeys = function(){
+// 	return Object.getOwnPropertyNames(student)
+// // Object.getOwnPropertyNames(Student.prototype)
+// }
+
+// // Object = instance of class student
+// const student = new Student("ruchi");
+// // const student = new Student(name)
+
+// // console.log(student.getKeys())
+// student.getKeys()
