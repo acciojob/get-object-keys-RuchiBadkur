8a6f7fbcff0824a4b12aaeba466112723e3e1b69
Ruchi Badkur <94377483+RuchiBadkur@users.noreply.github.com>
2024-12-19 12:52:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function Student(name){
+	this.name = name
+}
+
+const student = new Student("ruchi");
+// const student = new Student(name)
+student.getKeys = function(){
+	return Object.getOwnPropertyNames(student)
+// Object.getOwnPropertyNames(Student.prototype)
+}
+
+// console.log(student.getKeys())
+student.getKeys()
